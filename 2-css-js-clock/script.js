b3a55bbbf98caabc61db27b3b8c4514b0ddd8826
ARTUR ARSALANOV <kurumkan@ARTURs-MacBook-Air.local>
2017-07-06 12:08:03 +0300

    update min and hour arrow definition

--- 2-css-js-clock/script.js
+++ 2-css-js-clock/script.js
@@ -6,10 +6,15 @@ updateClock();
 setInterval(updateClock, 1000);
 
 function updateClock() {
-  const today = new Date();
-  const hAngle = 270 + (today.getHours() % 12) * 30;
-  const mAngle = 270 + (today.getMinutes() % 60) * 6;
-  const sAngle = 270 + (today.getSeconds() % 60) * 6;
+  const today = new Date(),
+        h = today.getHours(),
+        m = today.getMinutes(),
+        s = today.getSeconds();
+
+  const hAngle = 270 + (h % 12) * 30 + (m % 60 / 60) * 30;
+        mAngle = 270 + (m % 60) * 6 + (s % 60 / 60) * 6;
+        sAngle = 270 + (s % 60) * 6;
+
   hourArrow.style.transform = 'rotate(' + hAngle + 'deg)';
   minuteArrow.style.transform = 'rotate(' + mAngle + 'deg)';
   secondArrow.style.transform = 'rotate(' + sAngle + 'deg)';
